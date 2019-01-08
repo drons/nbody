3d9c227f0a3a26ec3564e111be8f315afcb84b91
drons <drons@list.ru>
2019-01-08 22:37:50 +0300

    Optimize heap index functions

--- nbody/nbody_space_heap_func.h
+++ nbody/nbody_space_heap_func.h
@@ -6,44 +6,57 @@ struct nbody_heap_func
 {
 	static index_t left_idx(index_t idx)
 	{
-		return 2 * idx + 1;
+		return (idx << 1) + 1;
 	}
 
 	static index_t rght_idx(index_t idx)
 	{
-		return 2 * idx + 2;
+		return (idx << 1) + 2;
 	}
 
 	static index_t parent_idx(index_t idx)
 	{
-		return (idx - 1) / 2;
+		return (idx - 1) >> 1;
+	}
+
+	static bool is_left(index_t idx)
+	{
+		return idx & 1;
+	}
+
+	static bool is_right(index_t idx)
+	{
+		return (idx & 1) == 0;
+	}
+
+	static index_t left2right(index_t idx)
+	{
+		return idx + 1;
 	}
 
 	static index_t next_down(index_t idx)
 	{
-		index_t	parent = parent_idx(idx);
-		index_t	rght = rght_idx(parent);
-		while(rght == idx)
+		// While index is 'right' -> go down
+		while(is_right(idx))
 		{
+			index_t parent = parent_idx(idx);
 			// We at root again. Stop traverse.
 			if(parent == 0)
 			{
 				return 0;
 			}
 			idx = parent;
-			parent = parent_idx(idx);
-			rght = rght_idx(parent);
 		}
-		return rght;
+		return left2right(idx);
 	}
 
 	static index_t skip_idx(index_t idx)
 	{
-		index_t	parent = parent_idx(idx);
-		index_t	left = left_idx(parent);
-		if(left == idx)
+		// Index is 'left' branch -> next index is 'right'.
+		// Simple take next one.
+		if(is_left(idx))
 		{
-			return rght_idx(parent);
+			return left2right(idx);
 		}
 		return next_down(idx);
 	}
@@ -55,7 +68,6 @@ struct nbody_heap_func
 		{
 			return left;
 		}
-
 		return next_down(idx);
 	}
 };
