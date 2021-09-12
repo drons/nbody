3eef91c9940dc6e383165779963b2abb216114e7
drons <drons@list.ru>
2021-09-12 23:12:39 +0300

    Add test for opencl_bh engine with two compute devices

--- test/engine/test_nbody_engine.cpp
+++ test/engine/test_nbody_engine.cpp
@@ -1529,6 +1529,25 @@ int main(int argc, char* argv[])
 									  1024, 1.6874e-13, 2);
 		res += QTest::qExec(&tc1, argc, argv);
 	}
+	{
+		QVariantMap param1(std::map<QString, QVariant>({{"engine", "simple_bh"},
+			{"distance_to_node_radius_ratio", 3.1623},
+			{"traverse_type", "cycle"},
+			{"tree_layout", "heap"},
+			{"tree_build_rate", 0}
+		}));
+		QVariantMap param2(std::map<QString, QVariant>({{"engine", "opencl_bh"},
+			{"device", "0:0;0:0"},
+			{"distance_to_node_radius_ratio", 3.1623},
+			{"traverse_type", "cycle"},
+			{"tree_layout", "heap"},
+			{"tree_build_rate", 0}
+		}));
+		test_nbody_engine_compare tc1(nbody_create_engine(param1),
+									  nbody_create_engine(param2),
+									  1024, 1.6874e-13, 2);
+		res += QTest::qExec(&tc1, argc, argv);
+	}
 #endif // HAVE_OPENCL
 	{
 		QVariantMap param1(std::map<QString, QVariant>({{"engine", "simple_bh"},
