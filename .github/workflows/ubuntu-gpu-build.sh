4fb400933f7999a23e2da926a85db50c8ef18596
drons <drons@list.ru>
2021-11-09 21:49:36 +0300

    Fix run at colab.research.google.com

--- .github/workflows/ubuntu-gpu-build.sh
+++ .github/workflows/ubuntu-gpu-build.sh
@@ -2,17 +2,18 @@
 
 set -eu
 
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64:./lib
+export LD_LIBRARY_PATH=/usr/lib64-nvidia:/usr/local/cuda/lib64:./lib
 export PATH=/usr/local/cuda/bin:$PATH
 
 mkdir build-root
 cd build-root 
 /usr/lib/x86_64-linux-gnu/qt5/bin/qmake ../nbody.pro \
-    CONFIG+=release "OPEN_CV_VERSION_MAJOR=4"
+    CONFIG+=release "OPEN_CV_VERSION_MAJOR=4" \
+    CONFIG+=NO_UI
 make -j2
 
-./test/engine/engine 
-./test/solver/solver 
+./test/engine/engine
+./test/solver/solver
 ./test/extrapolator/extrapolator
-./test/solvers_equality/solvers_equality 
+./test/solvers_equality/solvers_equality
 ./test/stream/stream
